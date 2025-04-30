02425bfa9a63378f00ddcea1cb37742fc5b0456d
Yudhi <baskyudh@gmail.com>
2025-04-30 13:46:11 +0700

    - Adjust import - Add testing codes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-import { BinarySearchTree } from "./bst.js";
+import { Tree, prettyPrint } from "./bst.js";
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+// console.log(tree.root);
+prettyPrint(tree.root);
+console.log(tree.insert(2));
+prettyPrint(tree.root);
