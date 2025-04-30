c8095c9f7f96864dfc0e3f681f71e64db17ed08d
Yudhi <baskyudh@gmail.com>
2025-04-30 13:48:12 +0700

    Major update to Node and Tree classes
    Node
    - Adjust Node class' left and right constructors to prevent infinite recursion
    - Adjust Node class' methods to reflect the changes
    
    Tree
    - Update name from BinarySearchTree to Tree
    - Set this.root constructor value to the sorted and deduplicated array parameter
    - Add buildTree method
    - Add insert method, which simply calls _insertRecursively
    
    Add prettyPrint to visualize Tree's root

--- bst.js
+++ bst.js
@@ -1,17 +1,85 @@
 class Node {
-  constructor(data, left, right) {
-    (this.data = data), (this.left = left), (this.right = right);
+  constructor(data, left = null, right = null) {
+    (this.data = data), (this._left = left), (this._right = right);
+  }
+  /**
+   * @param {Node|null} node
+   */
+  set left(node) {
+    return (this._left = node);
+  }
+  /**
+   * @param {Node|null} node
+   */
+  set right(node) {
+    return (this._right = node);
   }
 
-  setLeft(node) {
-    this.left = node;
+  get left() {
+    return this._left;
   }
 
-  setRight(node) {
-    this.right = node;
+  get right() {
+    return this._right;
   }
 }
 
-class BinarySearchTree {}
+class Tree {
+  constructor(array) {
+    (this.array = array),
+      (this.root = this.buildTree(
+        [...new Set(this.array)].sort((a, b) => a - b)
+      ));
+  }
+  buildTree(sortedArray) {
+    if (sortedArray.length === 0) return null;
+    const midIndex = Math.floor(sortedArray.length / 2);
+    const node = new Node(sortedArray[midIndex]);
 
-export { BinarySearchTree };
+    node.left = this.buildTree(sortedArray.slice(0, midIndex));
+    node.right = this.buildTree(sortedArray.slice(midIndex + 1));
+
+    return node;
+  }
+
+  insert(value) {
+    const [newRoot, inserted] = this._insertRecursively(this.root, value);
+    this.root = newRoot;
+    return inserted;
+  }
+
+  _insertRecursively(node, value) {
+    if (node === null) {
+      return [new Node(value), true];
+    }
+
+    if (value < node.data) {
+      const [newLeft, inserted] = this._insertRecursively(node.left, value);
+      node.left = newLeft;
+      return [node, inserted];
+    } else if (value > node.data) {
+      const [newRight, inserted] = this._insertRecursively(node.right, value);
+      node.right = newRight;
+      return [node, inserted];
+    } else {
+      return [node, false];
+    }
+  }
+
+  deleteItem(value) {}
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+export { Tree, prettyPrint };
