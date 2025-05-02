af5bb88ee7b7039b3f0ea4c0f46dccb47e636f9f
Yudhi <baskyudh@gmail.com>
2025-05-02 09:08:15 +0700

    Add code to test levelOrder method

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ import { Tree, prettyPrint } from "./bst.js";
 const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
 // console.log(tree.root);
+console.log(tree.insert(12));
 prettyPrint(tree.root);
-console.log(tree.insert(2));
-prettyPrint(tree.root);
+tree.levelOrder(tree.traverseAndPrint);
