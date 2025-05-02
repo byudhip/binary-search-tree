9470a4bfe4f748856ec9376a6a442cfc238200d3
Yudhi <baskyudh@gmail.com>
2025-05-02 09:54:15 +0700

    Add several methods calls for testing purposes - insert() method calls to remove balance from the tree - console.log inOrder() to see its return value

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import { Tree, prettyPrint } from "./bst.js";
 
 const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 
-// console.log(tree.root);
-console.log(tree.insert(12));
 prettyPrint(tree.root);
-tree.levelOrder(tree.traverseAndPrint);
+tree.insert(12);
+tree.insert(122);
+tree.insert(782);
+tree.insert(13000);
+tree.insert(5);
+prettyPrint(tree.root);
+// tree.levelOrder(tree.traverseAndPrint);
+console.log(tree.inOrder(() => {}));
